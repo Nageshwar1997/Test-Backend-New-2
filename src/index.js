5e533e9cd84857bbd8fc531d15ab8d89cbdfde12
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-25 21:29:31 +0530

    Test

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+app.get("/", function (req, res) { return res.send("Express on Vercel"); });
+app.listen(3000, function () { return console.log("Server ready on port 3000."); });
+module.exports = app;
